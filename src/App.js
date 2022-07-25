a7574c698f546dde5721eda6a4d9e070231a201d
= <=>
2022-07-26 01:34:55 +0530

    Added question mark

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ const faqs = [
       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
   },
   {
-    question: "What's the best thing about Switzerland?????",
+    question: "What's the best thing about Switzerland??",
     answer:
       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
   }
