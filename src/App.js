4c77c5141a9d35f4df39bc3e93dd88c1659acfc1
= <=>
2022-07-27 01:52:55 +0530

    Made final changes in the header

--- src/App.js
+++ src/App.js
@@ -333,10 +333,10 @@ function App() {
                     alt=""
                   />
                   <div className="absolute inset-0 w-full h-full flex items-center justify-center" aria-hidden="true">
-                    <svg className="h-20 w-20 text-indigo-500" fill="currentColor" viewBox="0 0 84 84">
+                    {/* <svg className="h-20 w-20 text-indigo-500" fill="currentColor" viewBox="0 0 84 84">
                       <circle opacity="0.9" cx={42} cy={42} r={42} fill="white" />
                       <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
-                    </svg>
+                    </svg> */}
                   </div>
                 </button>
               </div>
