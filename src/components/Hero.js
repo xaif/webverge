79c35c2fcfa3832b5fa8b6fa420f283e1024ed59
harshverma036 <harshverma0362@gmail.com>
2022-08-05 04:11:52 +0530

    conflict fixes done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -153,13 +153,8 @@ export default function Hero() {
                   ))}
                 </div>
                 <a
-<<<<<<< HEAD
                   href="#"
                   className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-=======
-                  href="/contact"
-                  className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
->>>>>>> 341306790b37808c4321b69c7fbcd98433fde51d
                 >
                   Log in
                 </a>
@@ -209,7 +204,6 @@ export default function Hero() {
                     type="submit"
                     className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
                   >
-<<<<<<< HEAD
                     Search Domain
                   </button>
                 </form>
@@ -296,104 +290,3 @@ export default function Hero() {
     </div>
   );
 }
-=======
-                    Log in
-                  </a>
-                  <a
-                    href="#"
-                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                  >
-                    Sign Up
-                  </a>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-<main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
-<div className="lg:grid lg:grid-cols-12 lg:gap-8">
-  <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
-    <h1>
-      <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
-        <span className="block text-gray-900">Next-Gen WordPress</span>
-        <span className="block text-indigo-600">Cloud Hosting</span>
-      </span>
-    </h1>
-    <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-    Kickstart your first Business website with Cloud SSD Storage. LiteSpeed powered server with the best performance & speed.
-    </p>
-    <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-      <p className="text-base font-medium text-gray-900">Find Your Perfect Domain Name</p>
-      <form action="https://account.webverge.io/cart.php" method="GET" target="_blank" className="mt-3 sm:flex">
-        <label htmlFor="text" className="sr-only">
-        text
-        </label>
-        <input                  
-          type="text"
-          name="query"   
-          required="true"
-          id="text"
-          className="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300"
-          placeholder="Enter your domain name"
-        />
-          <input type="hidden" name="a" value="add" />
-          <input type="hidden" name="domain" value="register" ></input>
-        <button
-          type="submit"
-          className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
-        >
-          Search Domain
-        </button>
-      </form>
-    </div>
-  </div>
-  <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-    <svg
-      className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden"
-      width={640}
-      height={784}
-      fill="none"
-      viewBox="0 0 640 784"
-      aria-hidden="true"
-    >
-      <defs>
-        <pattern
-          id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e"
-          x={118}
-          y={0}
-          width={20}
-          height={20}
-          patternUnits="userSpaceOnUse"
-        >
-          <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-        </pattern>
-      </defs>
-      <rect y={72} width={640} height={640} className="text-gray-50" fill="currentColor" />
-      <rect x={118} width={404} height={784} fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)" />
-    </svg>
-    <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
-      <button
-        type="button"
-        className="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        <span className="sr-only">Watch our video to learn more</span>
-        <img
-          className="w-full"
-          src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-          alt=""
-        />
-        <div className="absolute inset-0 w-full h-full flex items-center justify-center" aria-hidden="true">
-          <svg className="h-20 w-20 text-indigo-500" fill="currentColor" viewBox="0 0 84 84">
-            <circle opacity="0.9" cx={42} cy={42} r={42} fill="white" />
-            <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
-          </svg>
-        </div>
-      </button>
-    </div>
-  </div>
-</div>
-</main>
-</div>
-</div>
-);
-}
->>>>>>> 341306790b37808c4321b69c7fbcd98433fde51d
