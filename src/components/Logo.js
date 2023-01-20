a3a316e05292a42ace5887931014b84c9190d3ac
= <=>
2023-01-21 01:48:56 +0530

    Learnt how to add images

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,15 @@
+import iide from '../assets/IIDE_gray.svg'
+
 export default function Logo() {
     return (
       <div className="bg-gray-50">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
             <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-              <img className="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
+            <img className="h-12" src={iide} alt="IIDE Logo" />
             </div>
             <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-              <img className="h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />
+              <img className="h-12" src={iide} alt="IIDE" />
             </div>
             <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
               <img className="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />
